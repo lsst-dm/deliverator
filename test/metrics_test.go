a4d8406f877f2345f3cd096c17c45ddd63884dca
Joshua Hoblitt <josh@hoblitt.com>
2025-08-26 16:41:02 -0700

    add trivial /metrics endpoint integration test

---
+++ test/metrics_test.go
@@ -0,0 +1,21 @@
+package s3nd_test
+
+import (
+	"io"
+	"net/http"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("GET /metrics", func() {
+	It("returns 200", func() {
+		resp, err := http.Get(s3ndUrl.String() + "/metrics")
+		Expect(err).NotTo(HaveOccurred())
+		defer resp.Body.Close()
+		Expect(resp.StatusCode).To(Equal(http.StatusOK))
+
+		body, _ := io.ReadAll(resp.Body)
+		Expect(string(body)).To(ContainSubstring("s3nd_upload_parts_active"))
+	})
+})
